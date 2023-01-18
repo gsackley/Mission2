999db4eaa293becc8b8860b0aaef58003ac72718
garrett.sackley@gmail.com <garrett.sackley@gmail.com>
2023-01-17 20:36:44 -0700

    Changed Something

--- Mission2/Program.cs
+++ Mission2/Program.cs
@@ -7,6 +7,7 @@ namespace Mission2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Whats up?");
         }
     }
 }
